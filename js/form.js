317ce0e7ebd10af774cd8aaeaa66589030549553
Saad Hussain <saadatoad@gmail.com>
2025-04-16 21:02:29 +1000

    added send_maill functions for contact.html

--- js/form.js
+++ js/form.js
@@ -1,1 +1,22 @@
 // contact form validation/submission
+window.onload = function () {
+    emailjs.init({
+      publicKey: "cn8V8gjTY22xvh7_-",
+    });
+  
+    const form = document.getElementById('contact-form');
+    form.addEventListener('submit', send_email);
+};
+  
+async function send_email(event) {
+    event.preventDefault(); 
+    try {
+      await emailjs.sendForm('service_n1wrsw4', 'template_4pg2qd7', event.target);
+      alert('email sent.');
+      event.target.reset(); 
+    } catch (error) {
+      console.error('email not sent:', error);
+      alert('email not sent.');
+    }
+}
+  
