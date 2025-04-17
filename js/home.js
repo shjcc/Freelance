36bf78edd7157c1f568fe8f87e63c95f0f04a876
Saad Hussain <saadatoad@gmail.com>
2025-04-17 18:00:01 +1000

    refactor  typewriter effect into function

--- js/home.js
+++ js/home.js
@@ -1,12 +1,29 @@
 // functions for the home page
 
-// typewrite function
-new TypeIt(".hero-title", {
-    speed: 80,
-    deleteSpeed: 100,
-    loop: true,
-})
-    .type("I build websites.")
-    .delete()
-    .type("I also build cats?")
-    .go();
+// typewriter function
+function runTyping() {
+    new TypeIt(".hero-title", {
+      loop: false,
+      waitUntilVisible: true,
+      afterComplete: async (instance) => {
+        await instance.destroy();
+        setTimeout(runTyping, 500); 
+      },
+    })
+      .type("Hello!", { delay: 800 })
+      .delete(null, { delay: 500 })
+      .type("I am a Web")
+      .move(null, { to: "END" })
+      .type(' <span class="place">Developer</span>.', { delay: 800 })
+      .move(-10, { delay: 350 })
+      .move(11, { delay: 350 })
+      .delete(".place", { delay: 800 })
+      .type('<em><strong class="font-semibold"> Designer.</strong></em>', {
+        speed: 100,
+        delay: 800,
+      })
+      .delete(null, { delay: 500 })
+      .go();
+  }
+  
+  runTyping();
