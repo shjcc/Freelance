87ab51d348447336d1122d290314ee9ea246c3b8
Saad Hussain <saadatoad@gmail.com>
2025-04-17 17:15:19 +1000

    typewriter effect, updated styling for index.html

--- js/home.js
+++ js/home.js
@@ -1,1 +1,12 @@
 // functions for the home page
+
+// typewrite function
+new TypeIt(".hero-title", {
+    speed: 80,
+    deleteSpeed: 100,
+    loop: true,
+})
+    .type("I build websites.")
+    .delete()
+    .type("I also build cats?")
+    .go();
