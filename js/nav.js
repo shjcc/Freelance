21f169f183ecd697d7289693bc8e7181d41e2f55
Saad Hussain <saadatoad@gmail.com>
2025-04-19 22:25:52 +1000

    update navbar

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,12 @@
 // sticky headers and mobile nav toggles
+
+// detect current pages
+const currentLocation = window.location.pathname.split("/").pop();
+const navLinks = document.querySelectorAll(".nav-center a, .nav-right a");
+
+navLinks.forEach(link => {
+    const href = link.getAttribute("href");
+    if (href === currentLocation) {
+        link.classList.add("active");
+    }
+});
