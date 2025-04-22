9a6b2a2c0c3e94d24c6ef98adfe91e599d6143d3
Saad Hussain <saadatoad@gmail.com>
2025-04-22 19:41:25 +1000

    pre custom icon for navbar mobile

--- js/nav.js
+++ js/nav.js
@@ -10,3 +10,13 @@ navLinks.forEach(link => {
         link.classList.add("active");
     }
 });
+
+// mobile navbar 
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.querySelector(".mobile-menu");
+    const navCenter = document.querySelector(".nav-center");
+  
+    menuButton.addEventListener("click", () => {
+      navCenter.classList.toggle("open");
+    });
+});
