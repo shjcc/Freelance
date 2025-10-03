ce4018f96fae5d6c91d94e4bfe0253f834ca1c9f
Saad Hussain <saadatoad@gmail.com>
2025-10-03 18:35:49 +1000

    updated about.html new layout

--- js/about.js
+++ js/about.js
@@ -4,53 +4,5 @@ window.onload = function () {
     title.classList.add("fade-up");
 };
 
-// animate skills-grid section
-document.addEventListener("DOMContentLoaded", () => {
-    const aboutItems = document.querySelectorAll(".about-item");
 
-    const observerOptions = {
-        threshold: .1,
-    };
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-            } else {
-                entry.target.classList.remove("visible");
-            }
-        });
-    }, observerOptions);
-
-    aboutItems.forEach((item, index) => {
-        item.style.transitionDelay = `${index * 0.1}s`;
-        observer.observe(item);
-    });
-});
-
-// animate skills-grid section
-document.addEventListener("DOMContentLoaded", () => {
-    const skillsItems = document.querySelectorAll(".skills-item");
-    // const skillsTitle = document.querySelector(".skills-title");
-
-    const observerOptions = {
-        threshold: 1,
-    };
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-            } else {
-                entry.target.classList.remove("visible");
-            }
-        });
-    }, observerOptions);
-
-    // observer.observe(skillsTitle);
-
-    skillsItems.forEach((item, index) => {
-        item.style.transitionDelay = `${index * .05}s`;
-        observer.observe(item);
-    });
-});
